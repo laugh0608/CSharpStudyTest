5e91483fb3e0f97e162859adae0a9651d8dc891c
laugh0608 <laugh608@163.com>
2025-05-17 10:43:31 +0800

    学习了一下矩阵数据转换为向量数组

---
+++ CSharpStudyTest/20250517/MatrixTest.cs
@@ -0,0 +1,30 @@
+﻿
+using MathNet.Numerics.LinearAlgebra;
+
+namespace CsharpStudyTest._20250517;
+
+public class MatrixTest
+{
+    public void TestMatrix()
+    {
+        var m = Matrix<double>.Build;
+
+        var pArray = m.DenseOfArray(new[,]{
+            {1.0,2.0,1.0},
+            {-2.0,-3.0,1.0},
+            { 3.0,5.0,0.0}
+        });
+
+        pArray.Rank();
+        pArray.Range();
+
+        var v = pArray.Range();
+        
+        Console.WriteLine("Matrix rows as vectors:");
+        // 遍历数组 v 并输出每个向量
+        foreach (var rowVector in v)
+        {
+            Console.WriteLine(rowVector);
+        }
+    }
+}
--- CSharpStudyTest/Program.cs
+++ CSharpStudyTest/Program.cs
@@ -1,18 +1,18 @@
 ﻿
-namespace CsharpStudyTest
+namespace CsharpStudyTest;
+
+internal abstract class Program
 {
-    class Program
+    // Main() 函数必须是静态的
+    private static void Main()
     {
-        // Main() 函数必须是静态的
-        static void Main()
-        {
-            // 静态方法中不能调用非静态方法，除非通过创建类的实例实例来调用
-            // TestCode.Test19 obj = new TestCode.Test19();
-            // obj.TestFunction19();
-            // 在非静态方法或静态方法中调用静态方法应该通过类名直接调用，而不是通过实例对象调用
-            // TestCode.Test1.TestFunction1();
-            TestCode1.TestTemperature obj = new TestCode1.TestTemperature();
-            obj.TestFunction6();
-        }
+        // 静态方法中不能调用非静态方法，除非通过创建类的实例实例来调用
+        // TestCode.Test19 obj = new TestCode.Test19();
+        // obj.TestFunction19();
+        // 在非静态方法或静态方法中调用静态方法应该通过类名直接调用，而不是通过实例对象调用
+        // TestCode.Test1.TestFunction1();
+        
+        var obj = new _20250517.MatrixTest();
+        obj.TestMatrix();
     }
 }
